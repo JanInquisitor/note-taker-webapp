0e1e032cef72324f22fbbcd22804ea9693e592de
janinquisitor <jancarlos0195@gmail.com>
2022-03-29 23:43:45 -0400

    Changed the port for the server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 
 dotenv.config({path: "./config.env"});
 
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT || 3001, () => {
     console.log(`App running at port ${process.env.PORT}`);
 });
 
